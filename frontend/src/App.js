4cb6b524c67375ed27d766c64feb16f636f82754
DennisMaas <mail@dennismaas.de>
2020-10-16 19:27:38 +0200

    add comment for app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,29 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
 function App() {
-    // brauchen wir zum speichern der ToDo-Liste
+    // useState beginnt mit einem leeren Array, der State ist destructured in toDoList
+    // und setToDoList
     const [toDoList, setToDoList]= useState([]);
 
-    // neues ToDO-Item erstellen
+    // neues ToDo-Item erstellen
     function addNewToDo(toDo){
         console.log(toDo)
         console.log(toDoList)
         setToDoList([...toDoList,toDo])
     }
     function getList(){
-        axios.get("/api/todo")      // axios.get holt immer .response.data von der API
-            .then(response => response.data)        // nach .get dann die response.data auspacken und .data rausholen
-            .then(data => setToDoList(data))        // dann die .data and setToDoList übergeben.
+        axios.get("/api/todo")      // 2. axios.get holt immer .response.data von der API
+            .then(response => response.data)        // 2. nach .get dann die response.data auspacken und .data rausholen
+            .then(data => setToDoList(data))        // 2. dann die .data and setToDoList übergeben.
             .then(()=> console.log(toDoList))
             .catch(()=> console.log("Data fetching failed"))
 
     }
-
+    // 1. wenn useEffect leer ist, passiert es on first render
+    // 1. die function getList wird ausgeführt und holt die response.data
     useEffect(()=> {getList()},[])
 
     //Ausgabe auf dem Screen
-    //onAddToDo kann gelöscht werden?
   return (
     <div>
       <Header onAddToDo = {addNewToDo}></Header>
